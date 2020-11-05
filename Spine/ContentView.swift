ca0b0620f8969ee5c4f27f7eabd81a7b0198aa68
Gaurav Srivastava <srivastava.gaurava@gmail.com>
2020-11-05 19:38:38 +0530

    Initial Commit

---
+++ Spine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spine
+//
+//  Created by apple on 05/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
