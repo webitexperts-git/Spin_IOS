de4fe9421d1136943109a195bd0465432eaf1665
Gaurav Srivastava <srivastava.gaurava@gmail.com>
2020-11-05 23:13:19 +0530

    new folder for keyboard

--- Spine/ContentView.swift
+++ Spine/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+
+//               Color.blue.edgesIgnoringSafeArea(.all)
+            Color(red: 237 / 255, green: 215 / 255, blue: 183 / 255).edgesIgnoringSafeArea(.all)
+            VStack (spacing:10){
+                Text("SPINE").font(.largeTitle).foregroundColor(Color.white)
+                Text("The Spiritual Network").font(.subheadline).foregroundColor(Color.white).padding(.bottom, 100)
+                Text("Connect with people with your intrests.").font(.subheadline).foregroundColor(Color.white)
+                Text("Anytime.Anywhere.").font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                
+                
+                Button(action: {}) {
+                    Text("REGISTER")
+                    .frame(minWidth: 0, maxWidth: 200, minHeight: 0, maxHeight: 30)
+                }
+                .foregroundColor(Color(red: 237 / 255, green: 215 / 255, blue: 183 / 255))
+                .background(Color.white)
+                .cornerRadius(18)
+                .padding()
+                Button(action: {}) {
+                    Text("Login")
+                }
+                .foregroundColor(Color.white)
+                }
+
+
+           }
+             
     }
 }
 
